9fb84c64fdb8340398107978b718349e432b1137
emilisz <eciurlionis@gmail.com>
2022-02-19 13:29:30 +0200

    first commit7

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -3,7 +3,7 @@ import ImageGallery from 'react-image-gallery'
 
 
 const Gallery = ({images, t}) => {
-  return <ImageGallery className="mt-5" items={images} thumbnailPosition={t ? 'left' : 'bottom'} lazyLoad={true} showPlayButton={false} />
+  return <ImageGallery className="mt-5" items={images} key={images.id} thumbnailPosition={t ? 'left' : 'bottom'} lazyLoad={true} showPlayButton={false} />
 }
 
 export default Gallery
