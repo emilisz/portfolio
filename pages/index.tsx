26dbe066611e550c5805e8b5e6073867332e0bea
emilisz <eciurlionis@gmail.com>
2022-02-19 12:44:38 +0200

    first commit4

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import other1 from '../public/img/other1.webp'
 import other2 from '../public/img/other2.webp'
 import other3 from '../public/img/other3.webp'
 import other4 from '../public/img/other4.webp'
-import Gallery from './components/Gallery'
+import Gallery from '../components/Gallery'
 
 const otherImages = [
   {
